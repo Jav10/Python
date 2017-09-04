7352883fc21df47e4148c9687f0be889460d6287
Javier Arturo <punkrock.arturito@gmail.com>
2017-09-03 19:33:11 -0500

    Add files via upload

---
+++ entradaSalida.py
@@ -0,0 +1,14 @@
+#Entrada y Salida Estándar
+#Autor: Javier Arturo Hernández Sosa
+#Fecha: 3/Sep/2017
+#Descripcion: Curso Python FES Acatlán
+
+#Entrada y Salida Estándar
+#Salida
+x="Paco"
+print("Hola ",x) #print puede tener varios valores separados por coma
+print("Hola",x,"¿Cómo estas?")	
+print(f"Hola, {x}. ¿Cómo estas?")	#Notacón PEP 498
+#Entrada
+x = input("Dame un número: ")
+print(x)
